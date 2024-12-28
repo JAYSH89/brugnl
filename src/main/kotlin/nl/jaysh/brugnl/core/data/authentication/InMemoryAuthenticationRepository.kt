fc0cb2011479df51de0f8397df77381f1b7ba6d6
JAYSH <sanjaysheombar@gmail.com>
2024-12-28 03:03:41 +0100

    Add test helper data classes

--- src/main/kotlin/nl/jaysh/brugnl/core/data/authentication/InMemoryAuthenticationRepository.kt
+++ src/main/kotlin/nl/jaysh/brugnl/core/data/authentication/InMemoryAuthenticationRepository.kt
@@ -7,16 +7,37 @@ import org.springframework.stereotype.Repository
 @Repository
 class InMemoryAuthenticationRepository : AuthenticationRepository {
 
+    private val authenticationResponse = AuthenticationResponse(
+        kind = "identitytoolkit#VerifyPasswordResponse",
+        localId = "localId",
+        email = "test@example.com",
+        displayName = "",
+        idToken = "idToken",
+        registered = true,
+        refreshToken = "refreshToken",
+        expiresIn = "3600",
+    )
+
+    private val refreshResponse = RefreshResponse(
+        accessToken = "accessToken",
+        expiresIn = "3600",
+        tokenType = "Bearer",
+        refreshToken = "refreshToken",
+        idToken = "idToken",
+        userId = "userId",
+        projectId = "projectId",
+    )
+
     override suspend fun register(email: String, password: String): AuthenticationResponse {
-        TODO()
+        return authenticationResponse.copy(email = email)
     }
 
     override suspend fun login(email: String, password: String): AuthenticationResponse {
-        TODO()
+        return authenticationResponse.copy(email = email)
     }
 
     override suspend fun refresh(refreshToken: String): RefreshResponse {
-        TODO()
+        return refreshResponse
     }
 
     override suspend fun logout() {
